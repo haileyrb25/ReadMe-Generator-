766705d871b00b4a596736f49e1fea6f0a725e24
Hailey Bates <hailey.bates001@gmail.com>
2022-04-20 14:54:37 -0400

    added questions with inquirer

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+//insert questions
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+      
+      
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+      
+    },
+
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What kind of license does your project have?',
+    },
+    {
+        type: '',
+        name: '',
+        message: 'What languages were used in this project?',
+      },
+      {
+          type: 'input',
+          name: 'contributors',
+          message: 'Who contributed to this project?',
+      },
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
